77e610d92fe1bc3c48faf1c5755e18b1fbfd7d71
AsadAziz <Asadazizjs@gmail.com>
2017-03-21 15:51:52 +0500

    java assignment

---
+++ practice/app.js
@@ -0,0 +1,228 @@
+
+   /*
+
+
+
+
+                      Muhammad Waqas
+                      roll no# 5514
+
+*/
+
+
+
+// *************************Question no# 1**********************
+
+
+// var name = prompt("enter your name")
+// alert("Hi, " +name);
+
+
+
+// *************************End**********************
+
+
+
+
+
+// *************************Question no# 2**********************
+        var table= +prompt("Table",'5');
+        // var table2 = document.getElementsByClassName("table 2");
+        // var table3 = document.getElementsByClassName("table 3");
+        // var table4 = document.getElementsByClassName("table 4");
+        // var table5 = document.getElementsByClassName("table 5");
+        // var table2 = document.getElementsByClassName("table 6");
+      if (table === 2)
+{
+    console.log("hello");
+}
+
+// *************************End**********************
+
+
+
+
+// *************************Question no#3 **********************
+
+// var city = prompt("enter your city");
+// if (city === "karachi")
+// {
+//     alert('“Welcome to city of lights”');
+// }
+// else {
+
+// }
+
+// *************************End**********************
+
+
+
+
+
+
+// *************************Question no#4 **********************
+
+
+// var gen = prompt("enter gender");
+// if (gen === "male")
+// {
+//     console.log (" Good Morning Sir");
+// }
+// else if ( gen === "female")
+// {
+//     console.log( "Good Morning Ma’am");
+// }
+
+
+// *************************End**********************
+
+
+
+
+
+// *************************Question no#5 **********************
+
+
+// var trf = prompt("enter traffic signal");
+// if (trf === "red")
+// {
+//     console.log("vehicles must stop");
+// }
+// else if (trf === "yellow")
+// {
+//     console.log("vehicles should get ready to move");
+// }
+// else if (trf === "green")
+// {
+//     console.log("vehicles can move now");
+// }
+
+
+
+// *************************End**********************
+
+
+
+
+
+// *************************Question no#6 **********************
+
+// var age = prompt("enetr your age");
+// if (age >10 && age <26)
+// {
+//     console.log(' “You are welcome”');
+// }
+
+
+
+
+
+
+
+// *************************End**********************
+
+
+
+
+
+
+
+// *************************Question no#7 **********************
+
+
+// var fuel = prompt(" input remaining fuel in car (in litres)");
+// if (fuel < 0.25 ) 
+// {
+//     console.log(' “Please refill the fuel in your car”');
+// }
+
+
+
+
+
+// *************************End**********************
+
+
+
+
+
+
+// *************************Question no#8 **********************
+
+
+
+
+
+
+
+
+// *************************End**********************
+
+
+
+
+// *************************Question no#9 **********************
+
+// var math = +prompt("enter your maths marks");
+// var sci= +prompt("enter your science marks");
+// var his = +prompt("enter your history marks");
+// var obt = (math+sci+his);
+// var per = (obt/300*100);
+// var total = (300);
+// console.log("Total Marks : "+total);
+// console.log("Marks Obtained : " +obt);
+// console.log("Percentage : " +per);
+// if (per => 80 && per <= 100)
+// {
+//     console.log("Grade: A-one");
+//     console.log("Remarks: Excellent");
+// }
+// else if (per => 70 && per <= 79)
+// {
+//     console.log("Grade: A ");
+//     console.log("Remarks: Good");
+// }    
+// else if (per => 60 && per <= 69)
+// {
+//     console.log("Grade: B ");
+//     console.log("Remarks: You need to improve");
+// }    
+// else
+// {
+//     console.log("Grade: Fail ");
+//     console.log("Remarks: Sorry");
+// }    
+
+
+
+
+
+// *************************End**********************
+// *************************Question no#10 **********************
+// *************************End**********************
+// *************************Question no#11 **********************
+// *************************End**********************
+// *************************Question no#12 **********************
+// *************************End**********************
+// *************************Question no#13 **********************
+// *************************End**********************
+// *************************Question no#14 **********************
+// *************************End**********************
+// *************************Question no#15 **********************
+// *************************End**********************
+// *************************Question no#16 **********************
+// *************************End**********************
+// *************************Question no#17 **********************
+// *************************End**********************
+// *************************Question no#18 **********************
+// *************************End**********************
+// *************************Question no#19 **********************
+// *************************End**********************
+// *************************Question no#20 **********************
+// *************************End**********************
+// *************************Question no#21 **********************
+// *************************End**********************
+// *************************Question no#22 **********************
+// *************************End**********************
+// *************************Question no#23 **********************
+// *************************End**********************
